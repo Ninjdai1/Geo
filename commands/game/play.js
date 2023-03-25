5a0bcfd0033ccd8331730bc6df9e3761365c13ec
Ninjdai <ChickenTendo3@gmail.com>
2023-03-25 09:20:40 +0100

    Add /play command

---
+++ commands/game/play.js
@@ -0,0 +1,61 @@
+const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('play')
+        .setNameLocalizations({
+            'en-GB': 'play',
+            'fr': 'play',
+        })
+		.setDescription('Start a game')
+        .setDescriptionLocalizations({
+            'en-GB': 'Start a game',
+            'fr': 'Lancer une partie',
+        }),
+	async execute(interaction) {
+        const localetext = interaction.locale in text ? text[interaction.locale] : text['en-GB'];
+        const playEMBED = new EmbedBuilder()
+            .setTitle(localetext.title)
+            .setColor('Green')
+            .setDescription(localetext.description)
+        const btns = new ActionRowBuilder()
+            .setComponents([
+                new ButtonBuilder()
+                    .setCustomId(`game_trivia_${interaction.user.id}`)
+                    .setLabel('Trivia')
+                    .setStyle(ButtonStyle.Success)
+                    .setEmoji('⁉️'),
+                new ButtonBuilder()
+                    .setCustomId(`game_capital_${interaction.user.id}`)
+                    .setLabel('Guess the capital')
+                    .setStyle(ButtonStyle.Success)
+                    .setEmoji('🏦'),
+                new ButtonBuilder()
+                    .setCustomId(`game_country_${interaction.user.id}`)
+                    .setLabel('Guess the country')
+                    .setStyle(ButtonStyle.Success)
+                    .setEmoji('🗺️'),
+                new ButtonBuilder()
+                    .setCustomId(`game_flag_${interaction.user.id}`)
+                    .setLabel('Guess the flag')
+                    .setStyle(ButtonStyle.Success)
+                    .setEmoji('🏁'),
+            ])
+        await interaction.editReply({ embeds: [playEMBED], components: [ btns ] })
+    }
+};
+
+const text = {
+    'en-GB': {
+        title: 'Start a game',
+        description: 'Choose the game you want to play using buttons below !'
+    },
+    'en-US': {
+        title: 'Start a game',
+        description: 'Choose the game you want to play using buttons below !'
+    },
+    'fr': {
+        title: 'Lancer une partie',
+        description: 'Choisissez un jeu via les boutons ci-dessous !'
+    },
+}
